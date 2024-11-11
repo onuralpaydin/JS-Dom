0733ea2ee7d85c23727b7b32eb0a16cc444b9ad0
Onur Alp Aydın <o.a.aydin17@gmail.com>
2024-11-11 18:11:08 +0300

    dom getElementsByName demo example added

--- dom.js
+++ dom.js
@@ -1,9 +1,51 @@
+//getElementById
 const element = document.getElementById("getElementByIdParagraph");
 console.log(element);
-element.style.backgroundColor="red"
+element.style.backgroundColor = "red";
 // case sensitive if it doesn't match with an element it returns null
 //getElementById can only be used to select a single element because IDs must be unique.
-element.innerHTML="Lorem ipsum innerHtml."
-element.innerText="Lorem ipsum innerText."
-element.textContent="Lorem ipsum textcontent."
+element.innerHTML = "Lorem ipsum innerHtml.";
+element.innerText = "Lorem ipsum innerText.";
+element.textContent = "Lorem ipsum textcontent.";
 // All methods shown are correct ways to change the text content of an element.
+
+//getElementsByName
+// The name attribute can be used on the following elements:
+{
+  /* <button>	name
+<fieldset>	name
+<form>	name
+<iframe>	name
+<input>	name
+<map>	name
+<meta>	name
+<object>	name
+<output>	name
+<param>	name
+<select>	name
+<textarea>	name */
+}
+let elements = document.getElementsByName("language");
+//returns nodeList
+console.log(elements);
+
+//GetElementsByName demo example codes
+let btn = document.getElementById('btnRate');
+//output value will change
+let output = document.getElementById('output');
+//if click event occurs this funct. will check every rates in nodeList.According to rate.check property Output value will change.
+btn.addEventListener('click', () => {
+    let rates = document.getElementsByName('rate');
+    //nodeList which include inputs that have name attribute rate.
+    console.log(rates);
+    rates.forEach((rate) => {
+        //every input. Not NodeList.
+        console.log(rate);
+        //true false value.If radiobutton checked it will return true
+        console.log(rate.checked);
+        if (rate.checked) {
+            output.innerText = `You selected: ${rate.value}`;
+        }
+    });
+
+});
