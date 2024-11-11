f34aca94200971ccf6e29f9fa013130347ca895e
Onur Alp Aydın <o.a.aydin17@gmail.com>
2024-11-11 16:34:23 +0300

    innerHtml , innerText , textContent examples added.

--- dom.js
+++ dom.js
@@ -1,4 +1,9 @@
 const element = document.getElementById("getElementByIdParagraph");
-element.innerHTML="Lorem ipsum."
-element.style.backgroundColor="red"
 console.log(element);
+element.style.backgroundColor="red"
+// case sensitive if it doesn't match with an element it returns null
+//getElementById can only be used to select a single element because IDs must be unique.
+element.innerHTML="Lorem ipsum innerHtml."
+element.innerText="Lorem ipsum innerText."
+element.textContent="Lorem ipsum textcontent."
+// All methods shown are correct ways to change the text content of an element.
