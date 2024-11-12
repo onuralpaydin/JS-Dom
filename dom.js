8384ff76cbab5fbe85e28cac52c4c2a4de58dcc1
Onur Alp Aydın <o.a.aydin17@gmail.com>
2024-11-12 22:57:10 +0300

    getElementsByTagName() added

--- dom.js
+++ dom.js
@@ -30,22 +30,32 @@ let elements = document.getElementsByName("language");
 console.log(elements);
 
 //GetElementsByName demo example codes
-let btn = document.getElementById('btnRate');
+let btn = document.getElementById("btnRate");
 //output value will change
-let output = document.getElementById('output');
+let output = document.getElementById("output");
 //if click event occurs this funct. will check every rates in nodeList.According to rate.check property Output value will change.
-btn.addEventListener('click', () => {
-    let rates = document.getElementsByName('rate');
-    //nodeList which include inputs that have name attribute rate.
-    console.log(rates);
-    rates.forEach((rate) => {
-        //every input. Not NodeList.
-        console.log(rate);
-        //true false value.If radiobutton checked it will return true
-        console.log(rate.checked);
-        if (rate.checked) {
-            output.innerText = `You selected: ${rate.value}`;
-        }
-    });
+btn.addEventListener("click", () => {
+  let rates = document.getElementsByName("rate");
+  //nodeList which include inputs that have name attribute rate.
+  console.log(rates);
+  rates.forEach((rate) => {
+    //every input. Not NodeList.
+    console.log(rate);
+    //true false value.If radiobutton checked it will return true
+    console.log(rate.checked);
+    if (rate.checked) {
+      output.innerText = `You selected: ${rate.value}`;
+    }
+  });
+});
+let btnCount = document.getElementById("btnCount");
+btnCount.addEventListener("click", () => {
+    //it returns html collection which includes all h2 elements
+  let headings = document.getElementsByTagName("h2");
+  console.log(headings);
+// also we can convert it to an array by using spread operator
+const convertedHtmlCollection=[...headings]
+console.log(convertedHtmlCollection);
 
+  alert(`H2 tag count: ${headings.length}`);
 });
